6c8d2d1e90630c0dfdce5d369058f7c7481d8ca4
Miguel Coba <miguel.coba@gmail.com>
2022-05-08 23:53:54 +0200

    Adds node details

--- lib/curiosum_meetup_web/controllers/page_controller.ex
+++ lib/curiosum_meetup_web/controllers/page_controller.ex
@@ -2,6 +2,8 @@ defmodule CuriosumMeetupWeb.PageController do
   use CuriosumMeetupWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    this_node = node()
+    other_nodes = Node.list()
+    render(conn, "index.html", %{this_node: this_node, other_nodes: other_nodes})
   end
 end
